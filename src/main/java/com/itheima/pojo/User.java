2be031c131953a106d48a82c648c2f592611380e
admin <1007942602@qq.com>
2019-10-11 21:17:11 +0800

    添加构造方法

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -4,5 +4,12 @@ public class User {
 
     private String username;//用户名
     private String password;//密码
-    
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
 }
